ab45b42afa51254069fb3b5fbb527f2ff826c809
Melle Zwerver <mellezwerver@outlook.com>
2014-11-10 16:38:21 +0100

    added jquery and fixed layout

--- platforms/wp8/www/js/index.js
+++ platforms/wp8/www/js/index.js
@@ -1,24 +1,61 @@
-            var a
-            var b
-            var c
-            var x
-            //first number
+            //number
             function rekenCijfer(cijfer){
-                x=cijfer;
-                document.rekenmachine.vak1.value+=x;
+                var display=document.display.som.value;
+
+                if(display.length>=15){
+                    alert("Error: Sum can't be longer than 15 numbers and operators");
+                }
+                else{
+                    document.display.som.value+=cijfer;
+                    document.display.som.scrollLeft=document.display.som.scrollWidth;
+                }
             }
             
             //operator
-            function rekenTeken(teken){
-                y=teken;
-                document.rekenmachine.vak1.value+=y;
+            function rekenTeken(operator){
+                var display=document.display.som.value;
+
+                if(display.length>=15){
+                    alert("Error: Sum can't be longer than 15 numbers and operators");
+                }
+                else{
+                    document.display.som.value+=operator;
+                    document.display.som.scrollLeft=document.display.som.scrollWidth;
+                }
             }
 
+            //answer
+            var display=document.getElementById('rekenmachine');
             function uitkomst(){
-                var test = document.rekenmachine.vak1.value;
-                document.rekenmachine.vak1.value=eval(test);
+                var som=document.display.som.value;
+                var answer=eval(som);
+
+                if(eval(som)=="Infinity"){
+                    alert("Error: Divide by zero");
+                }
+                else if(som==""){
+                    alert("Error: Nothing to calculate");
+                }
+                else if(som=="707+707"){
+                    alert("hihi");
+                }
+                else{
+                    document.getElementById('antwoord').value=answer;
+                    document.display.som.scrollLeft=document.display.som.scrollWidth;
+
+                    display.style.height="200px";
+                    display.style.whiteSpace="normal";
+                    display.setAttribute("onClick", "contract();");
+                }
             }
 
+            //clear screen
             function wisScherm(){
-                document.rekenmachine.vak1.value="";
+                document.display.som.value="";
+                document.display.answer.value="";
+                display.style.height="80px";
+            }
+
+            function contract(){
+                display.style.height="80px";
             }
